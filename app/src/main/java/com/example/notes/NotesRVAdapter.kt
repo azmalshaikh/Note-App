7f8a466f9e8b7b093312787003e2b69fb5cac132
Azmal Shaikh <azmalshaikh907@gmail.com>
2020-12-20 18:46:19 +0530

    initial commit

---
+++ app/src/main/java/com/example/notes/NotesRVAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.notes
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesRVAdapter(private val context: Context, private val listener: NoteItemClicked) : RecyclerView.Adapter<NotesRVAdapter.NoteViewHolder>() {
+
+    private val allNotes = ArrayList<Note>()
+
+    inner class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView = itemView.findViewById(R.id.text)
+        val deleteButton: ImageView = itemView.findViewById(R.id.deleteButton)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_note, parent, false)
+        val viewHolder = NoteViewHolder(view)
+        viewHolder.deleteButton.setOnClickListener{
+            listener.onItemClicked(allNotes[viewHolder.adapterPosition])
+        }
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = allNotes[position]
+        holder.textView.text = currentNote.text
+    }
+
+    override fun getItemCount(): Int {
+        return allNotes.size
+    }
+
+    fun update(newList: List<Note>) {
+        allNotes.clear()
+        allNotes.addAll(newList)
+        notifyDataSetChanged()
+    }
+}
+
+interface NoteItemClicked {
+    fun onItemClicked(note: Note)
+}
